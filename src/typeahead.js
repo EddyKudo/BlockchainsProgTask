159372cc14308bb908053b68e5e52f0ef8883865
Eduard Chiticari <eduard.kitikari@gmail.com>
2020-07-09 20:18:27 +0000

    fixed autosuggest starting from first letter

--- src/typeahead.js
+++ src/typeahead.js
@@ -1,34 +1,32 @@
 import React, { useState, useRef, useEffect} from "react";
 import PropTypes from 'prop-types';
-// import {colorsList} from "./index"
 
 export const Typeahead = (props) => {
 
     const [display, setDisplay] = useState(false);
     const [search, setSearch] = useState("");
-    const options = props.list;
-    let optionsLow = options.map((i)=>i.toLowerCase())
+    const options = props.list.map((i)=>i.toLowerCase());
+    // let optionsLow = options.map((i)=>i.toLowerCase());
+    const boxAroud = useRef(null);
 
     const setColorTab  = (color) => {
         setSearch(color);
         setDisplay(false);
-    }
+    };
 
-    const boxAroud = useRef(null);
-    
     useEffect(()=>{
         document.addEventListener("mousedown", handleClickOutside)
         return (()=>{
             document.removeEventListener("mousedown", handleClickOutside)
         })
-    },[])
+    },[]);
 
     const handleClickOutside = (e) =>{
         const {current: wrap} = boxAroud;
         if (wrap && !wrap.contains(e.target)){
             setDisplay(false);
         }
-    }
+    };
 
     return(
             <div className="wrap" >
@@ -37,16 +35,16 @@ export const Typeahead = (props) => {
                 <input id="searchField" value={search}  onChange={(e)=>setSearch(e.target.value)} onClick={()=>setDisplay(!display)} placeholder="Start typing color name.."></input>
                 {display && (
                     <div className="typeAheadContainer" ref={boxAroud}>
-                        {optionsLow.filter((i)=>i.indexOf(search.toLowerCase())>0).map((i)=>{
+                        {options.filter((i)=>i.toLowerCase().indexOf(search.toLowerCase())>-1).map((i)=>{
                             return <div onClick={()=> setColorTab(i)} tabIndex="0">
-                                        <span>{i}</span>
+                                        <div className ="colorsLine">{i}</div>
                                     </div>
                         })}
                     </div>
                 )}
             </div>
     )
-}
+};
 Typeahead.propTypes = {
     list : PropTypes.array
 };
