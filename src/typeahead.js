22af7103310faea1af2d4318cdc88a9fa6ce0344
Eduard Chiticari <eduard.kitikari@gmail.com>
2020-07-14 15:10:52 +0000

    fixed styling, cleaned up the code

--- src/typeahead.js
+++ src/typeahead.js
@@ -6,14 +6,20 @@ export const Typeahead = (props) => {
 
     const [display, setDisplay] = useState(false);
     const [search, setSearch] = useState("");
-    const options = props.list;
     const boxAroud = useRef(null);
+    const options = props.list;
 
     const setColorTab  = (color) => {
         setSearch(color);
         setDisplay(false);
     };
 
+    const handleClickOutside = (event) =>{
+        const {current: wrap} = boxAroud;
+        if (wrap && !wrap.contains(event.target)){
+            setDisplay(false);
+        }
+    };
     useEffect(()=>{
         document.addEventListener("mousedown", handleClickOutside)
         return (()=>{
@@ -21,19 +27,11 @@ export const Typeahead = (props) => {
         })
     },[]);
 
-    const handleClickOutside = (e) =>{
-        const {current: wrap} = boxAroud;
-        if (wrap && !wrap.contains(e.target)){
-            setDisplay(false);
-        }
-    };
-
     const escFunction = useCallback((event) => {
         if(event.keyCode === 27) {
         setDisplay(false);
         }
     }, []);
-
     useEffect(() => {
         document.addEventListener("keydown", escFunction, false);
         return () => {
@@ -50,14 +48,14 @@ export const Typeahead = (props) => {
 
     return(
             <div className="wrap" >
-                <h3>TYPEAHEAD INPUT COMPONENT</h3>
-                <p><b>BLOCKCHAINS</b> Programming Task</p>
-                <input id="searchField" value={search} onChange={(e)=>setSearch(e.target.value)} onClick={()=>setDisplay(!display)} placeholder="Start typing color name.."></input>
+                <h3>TypeAhead Input Component</h3>
+                <p>BLOCKCHAINS Programming Task</p>
+                <input autocomplete="off" id="searchField" value={search} onChange={(event)=>setSearch(event.target.value)} onClick={()=>setDisplay(!display)} placeholder="Start typing color name.."></input>
                 {display && search !== "" ?(
                     <div className="typeAheadContainer" ref={boxAroud}>
-                        {options.filter((i)=>i.toLowerCase().indexOf(search.toLowerCase())>-1).map((i)=>{                   
-                            return <div onKeyDown={(event)=>handleEnterKey(event,i)} onClick={()=> setColorTab(i)} tabIndex="0">
-                                        {<span dangerouslySetInnerHTML={{ __html: i.replace(new RegExp(search, "gi"), (match) => `<b>${match}</b>`) }} />}
+                        {options.filter((colorItem)=>colorItem.toLowerCase().indexOf(search.toLowerCase())>-1).map((colorItem)=>{                   
+                            return <div onKeyDown={(event)=>handleEnterKey(event,colorItem)} onClick={()=> setColorTab(colorItem)} tabIndex="0">
+                                        {<span dangerouslySetInnerHTML={{ __html: colorItem.replace(new RegExp(search, "gi"), (match) => `<b style="color:black;">${match}</b>`) }} />}
                                     </div>
                         })}
                     </div>
